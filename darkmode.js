18f9b688a63c186c5e22ca704d0b14a8d7240887
lucasdu-lab <ldu6509@gmail.com>
2024-11-29 18:45:33 +0800

    add math formatting and code highlighting

--- darkmode.js
+++ darkmode.js
@@ -1,11 +1,15 @@
 "use strict";
 // Toggle dark mode
 let colorSelect = document.getElementById("color");
+let codeHighlight = document.getElementById("highlight");
 colorSelect.addEventListener("change", function() {
 let body = document.querySelector("html");
 if (colorSelect.value === "dark") {
     body.setAttribute("data-bs-theme","dark");
+    codeHighlight.setAttribute("src","https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.4.0/styles/atom-one-dark.min.css");
 } else {
+    codeHighlight.setAttribute("src","https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.4.0/styles/atom-one-light.min.css");
     body.setAttribute("data-bs-theme","light");
 }
+hljs.highlightAll();
 });
