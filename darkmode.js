180eeb78175eb084c1c602a5c770b27c6783b024
lucasdu-lab <ldu6509@gmail.com>
2025-02-21 14:03:02 -0500

    add loops.html

--- darkmode.js
+++ darkmode.js
@@ -6,9 +6,9 @@ colorSelect.addEventListener("change", function() {
 let body = document.querySelector("html");
 if (colorSelect.value === "dark") {
     body.setAttribute("data-bs-theme","dark");
-    codeHighlight.setAttribute("src","https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.4.0/styles/atom-one-dark.min.css");
+    codeHighlight.setAttribute("href","https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/styles/github-dark.min.css");
 } else {
-    codeHighlight.setAttribute("src","https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.4.0/styles/atom-one-light.min.css");
+    codeHighlight.setAttribute("href","https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.4.0/styles/atom-one-light.min.css");
     body.setAttribute("data-bs-theme","light");
 }
 hljs.highlightAll();
